11c5d00d1c5bdfe1ee231a71a2c52df2f0e54386
penghui <penghui.zhang@foxmail.com>
2025-12-19 23:04:44 +0800

    feat(hex): hex subcommand to view single hex file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod cli;
 pub mod echo;
 pub mod grep;
+pub mod hex;
 pub mod view;
 
 pub use cli::{CliError, Parser, Result};
 pub use echo::{EchoArgs, EchoError, echo};
 pub use grep::{GrepArgs, GrepError, grep};
+pub use hex::{HexArgs, view_hex};
 pub use view::{ViewArgs, ViewError, view_file};
