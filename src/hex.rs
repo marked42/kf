cecf334009818de3cdcdf85cba218c439872bad2
zhangpenghui <zhangpenghui@kuaishou.com>
2025-12-22 20:10:34 +0800

    fix: last line without enough bytes of buffer length is missing

--- src/hex.rs
+++ src/hex.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::Read, path::PathBuf};
+use std::{
+    fs::File,
+    io::{ErrorKind, Read},
+    path::PathBuf,
+};
 
 use clap::{Parser, builder::RangedU64ValueParser};
 use thiserror::Error;
@@ -31,17 +35,42 @@ pub fn view_hex(args: HexArgs) -> Result<()> {
     let mut pos = 0;
     let mut buffer = vec![0; args.bytes_per_line];
 
-    while let Ok(_) = f.read_exact(&mut buffer) {
-        print!("[0x{:08x}] ", pos);
-        pos += args.bytes_per_line;
-        for byte in &buffer {
-            match *byte {
-                0x00 => print!(". "),
-                0xff => print!("## "),
-                _ => print!("{:02x} ", byte),
+    loop {
+        match f.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => {
+                buffer.resize(n, 0);
+                print!("[0x{:08x}] ", pos);
+
+                for i in 0..n {
+                    match buffer[i] {
+                        0x00 => print!(". "),
+                        0xff => print!("## "),
+                        _ => print!("{:02x} ", buffer[i]),
+                    }
+                }
+
+                for _ in n..args.bytes_per_line {
+                    print!("   ");
+                }
+
+                print!("  ");
+
+                for i in 0..n {
+                    let c = buffer[i] as char;
+                    if c.is_ascii_graphic() || c == ' ' {
+                        print!("{}", c);
+                    } else {
+                        print!(".");
+                    }
+                }
+
+                println!();
+                pos += n;
             }
+            Err(e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e.into()),
         }
-        println!();
     }
 
     Ok(())
